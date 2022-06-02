df7ac76e24090ec29791fde6ffe38ac38745a150
Paulo Fernando Oliveira <paulo.fdo@hotmail.com>
2022-06-01 22:33:24 -0300

    Function Hello World em dotnet (#1)
    * HelloWorld em .NET
    
    * Implementado Worklow do HelloWorld .NET Core
    
    * Movido yaml de Workflow
    
    * Corrigido branch

---
+++ dotnet/helloworld/Function.cs
@@ -0,0 +1,14 @@
+using Google.Cloud.Functions.Framework;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Function : IHttpFunction
+    {
+        public async Task HandleAsync(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello World!");
+        }
+    }
+}
